e15cf72682c0668ee82afe00dbee84a91ef9b7d0
shyam <shyamsrivastav47@gmail.com>
2024-01-13 20:50:34 +0530

    Adding comments to understand the code

--- index.js
+++ index.js
@@ -1,9 +1,19 @@
+//Fetching the html element
+
 let count=document.querySelector('.number');
+
+//convert the string into integer using parseInt
+
 let number=parseInt(count.innerText);
+
+//Function to decrement the value
+
 function decrement(){
     number=number-1;
     count.innerText=number;
 }
+
+//Function to increment the value
 
 function increment(){
     number=number+1;
